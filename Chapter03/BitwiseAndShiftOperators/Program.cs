446b8b694bb9be4069c7231ba89c00cf8853858d
Biplav Poudel <biplavpoudel764@gmail.com>
2023-06-20 19:44:32 +0545

    Selection Statements added

--- Chapter03/BitwiseAndShiftOperators/Program.cs
+++ Chapter03/BitwiseAndShiftOperators/Program.cs
@@ -5,3 +5,38 @@ WriteLine($"b = {b}");
 WriteLine($"a & b = {a & b}"); // 2-bit column only e.g. 00000010
 WriteLine($"a | b = {a | b}"); // 8, 4, and 2-bit columns e.g. 00001110
 WriteLine($"a ^ b = {a ^ b}"); // 8 and 4-bit columns e.g. 00001100
+
+// 01010000 left-shift a by three bit columns
+WriteLine($"a << 3 = {a << 3}");   //faster than multiplying by 8
+// multiply a by 8
+WriteLine($"a * 8 = {a * 8}");
+// 00000011 right-shift b by one bit column
+WriteLine($"b >> 1 = {b >> 1}");   //faster than diving by 2
+
+static string ToBinaryString(int value)
+{
+    return Convert.ToString(value, toBase: 2).PadLeft(8, '0');
+}
+
+
+WriteLine();
+WriteLine("Outputting integers as binary:");
+WriteLine($"a =     {ToBinaryString(a)}");
+WriteLine($"b =     {ToBinaryString(b)}");
+WriteLine($"a & b = {ToBinaryString(a & b)}");
+WriteLine($"a | b = {ToBinaryString(a | b)}");
+WriteLine($"a ^ b = {ToBinaryString(a ^ b)}");
+WriteLine();
+int age = 50;
+WriteLine($"The {nameof(age)} variable uses {sizeof(int)} bytes of memory.");
+
+char firstDigit = age.ToString()[0];
+WriteLine($"{firstDigit}");
+
+char secondDigit = age.ToString()[1];
+WriteLine(secondDigit);
+// There are four operators:
+// = is the assignment operator
+// . is the member access operator
+// () is the invocation operator
+// [] is the indexer access operator
