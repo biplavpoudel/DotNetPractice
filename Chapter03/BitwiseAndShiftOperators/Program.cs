3f46d5f9e21f80c6948bb35b2f8035b6317b7442
Biplav Poudel <biplavpoudel764@gmail.com>
2023-06-20 18:22:12 +0545

    Added Operators Project

---
+++ Chapter03/BitwiseAndShiftOperators/Program.cs
@@ -0,0 +1,7 @@
+﻿int a = 10; // 00001010
+int b = 6;  // 00000110
+WriteLine($"a = {a}");
+WriteLine($"b = {b}");
+WriteLine($"a & b = {a & b}"); // 2-bit column only e.g. 00000010
+WriteLine($"a | b = {a | b}"); // 8, 4, and 2-bit columns e.g. 00001110
+WriteLine($"a ^ b = {a ^ b}"); // 8 and 4-bit columns e.g. 00001100
---
+++ Chapter03/Operators/Program.cs
@@ -0,0 +1,19 @@
+﻿bool a = true;
+bool b = false;
+
+WriteLine($" {a^a}");
+WriteLine($" {a ^ b}");
+WriteLine($" {b ^ a}");
+WriteLine($" {b ^ b}");    //^ is XOR operator
+
+static bool DoStuff()
+{
+    WriteLine("I am doing some stuff.");
+    return true;
+}
+
+WriteLine();
+WriteLine($"a & DoStuff() = {a & DoStuff()}");   //this statement won't be printed unless the DoStuff() is executed. DoStuff() returns true and then only the statement is allowed to execute.
+WriteLine($"b & DoStuff() = {b & DoStuff()}");
+WriteLine();
+WriteLine($"b && DoStuff() = {b && DoStuff()}");  //short-circuiting i.e. b is deemed false and DoStuff() is not executed
