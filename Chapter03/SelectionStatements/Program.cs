0334c1e7718060d44108d23bbbbdc973c545dfee
Biplav Poudel <biplavpoudel764@gmail.com>
2023-06-20 19:45:38 +0545

    Selection Statements Added

---
+++ Chapter03/SelectionStatements/Program.cs
@@ -0,0 +1,11 @@
+﻿object o = "3";
+o = 3;
+int j = 4;
+if (o is int i)    //pattern matching; 'is' operator checks if 'o' can be cast to int. if cast successful, assignment is done inherently.
+{
+    WriteLine($"{i} x {j} = {i * j}");
+}
+else
+{
+    WriteLine("o is not an int so it cannot multiply!");
+}
