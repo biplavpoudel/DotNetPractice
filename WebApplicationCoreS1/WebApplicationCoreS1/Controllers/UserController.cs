554e9a35e77b678fdb00199be1cb52542c227031
Biplav Poudel <biplavpoudel764@gmail.com>
2023-07-15 16:42:13 +0545

    Model Binding Practiced

---
+++ WebApplicationCoreS1/WebApplicationCoreS1/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationCoreS1.Models;   //natra error aauxa."WebUser" model pani directive bata pass garnu paryo
+
+
+namespace WebApplicationCoreS1.Controllers
+{
+	public class UserController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult SimpleBinding()
+		{
+			return View(new WebUser() { FirstName = "Biplav", LastName= "Poudel"});
+		}
+
+		[HttpPost]      //notice action verb here: HttpPost
+		public IActionResult SimpleBinding(WebUser webUser)
+		{
+			return Content($"User {webUser.FirstName} updated!");
+		}
+	}
+}
