8dcbe2ce3f5da2bdbcb93f209f9edacf0f1bcca5
Biplav Poudel <biplavpoudel764@gmail.com>
2023-07-17 09:30:11 +0545

    Some leftovers from Model Validations

---
+++ WebApplicationCoreS1/WebApplicationCoreS1/Controllers/ValidationController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationCoreS1.Models;
+
+namespace WebApplicationCoreS1.Controllers
+{
+	public class ValidationController : Controller
+	{
+
+		public IActionResult Index(WebUser webUser)
+		{
+			//var webUser = TempData["WebUser"] as WebUser;
+			if (webUser == null)
+			{
+				return RedirectToAction("Index", "User");
+			}
+
+			return View(webUser);
+		}
+
+		[HttpPost]
+		public IActionResult ProcessForm(WebUser webUser)
+		{
+			if (ModelState.IsValid)
+			{
+				return Content("Form data validated!");
+			}
+			else
+			{
+				//ModelState.AddModelError("", "An error occurred.");
+				return Content("Form data not validated! Please enter again!");
+			}
+		}
+	}
+}
