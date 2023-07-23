5d583e33e2fd6fcb58211166a9cb9efaf59329b2
Biplav Poudel <biplavpoudel764@gmail.com>
2023-07-23 21:12:15 +0545

    Add an Entity model

---
+++ WorkingWithEntityFrameworkCore/WorkingWithEntityFrameworkCore/Models/WebUser.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WorkingWithEntityFrameworkCore.Models
+{
+	public class WebUser
+	{
+		[Column("UserID")]
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int UserID { get; set; }
+
+		[Display(Name = "First Name")]
+		[Required]
+		[StringLength(25)]
+		public string FirstName { get; set;}
+
+		[Required]
+		[StringLength(25),MinLength(3)]
+		[Display(Name = "Last Name")]
+		public string LastName { get; set;}
+
+		public string Email { get; set;}
+
+	}
+}
